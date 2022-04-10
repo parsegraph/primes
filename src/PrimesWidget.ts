9f52b9c5dfb54b7ff5ecef7577810489367aa2f1
Aaron Faanes <dafrito@gmail.com>
2022-04-10 02:06:00 -0500

    Fix deps

--- src/PrimesWidget.ts
+++ src/PrimesWidget.ts
@@ -1,21 +1,19 @@
 import PrimesModulo from "./PrimesModulo";
 import { DirectionCaret } from "parsegraph-direction";
 import Block from "parsegraph-block";
-import { Freezer } from "parsegraph-artist";
+import { BlockCaret } from "parsegraph-block";
 
 export default class PrimesWidget {
   position: number;
   knownPrimes: PrimesModulo[];
   caret: DirectionCaret<Block>;
   _paused: boolean;
-  _freezer: Freezer;
 
-  constructor(caret: DirectionCaret<Block>, freezer: Freezer) {
-    this._freezer = freezer;
+  constructor() {
     this.knownPrimes = [];
     this.position = 2;
 
-    this.caret = caret;
+    this.caret = new BlockCaret();
     this.caret.node().value().setLabel("1");
   }
 
@@ -24,23 +22,20 @@ export default class PrimesWidget {
   }
 
   freezer() {
-    return this._freezer;
+    return this.caret.freezer();
   }
 
   step() {
     // console.log("Stepping primes widget");
     // Check if any known prime is a multiple of the current position.
     this.caret.spawnMove("f", "b");
-    this.caret
-      .node()
-      .value()
-      .setLabel("" + this.position);
-    this.caret.node().state().setId(this.position);
+    this.caret.label("" + this.position);
+    this.caret.id(this.position);
     this.caret.push();
     this.caret.pull("u");
     this.caret.crease();
-    const freeze = !!this._freezer;
-    freeze && this.caret.node().value().getCache().freeze(this.freezer());
+    const freeze = !!this.freezer();
+    freeze && this.caret.freeze();
     let isPrime = true;
 
     for (let i = 0; i < this.knownPrimes.length; ++i) {
@@ -63,20 +58,14 @@ export default class PrimesWidget {
         .setId(this.position + ":" + prime.frequency);
       if (i === 0) {
         this.caret.crease();
-        freeze && this.caret.node().value().getCache().freeze(this.freezer());
+        freeze && this.caret.freeze();
       }
     }
     if (isPrime) {
       // The position is prime, so output it and add it to the list.
       this.caret.spawnMove("u", "b");
-      this.caret
-        .node()
-        .value()
-        .setLabel("" + this.position);
-      this.caret
-        .node()
-        .state()
-        .setId(this.position + ":" + this.position);
+      this.caret.label("" + this.position);
+      this.caret.id(this.position + ":" + this.position);
       this.knownPrimes.push(new PrimesModulo(this.position));
     }
     this.caret.pop();
--- src/demo.ts
+++ src/demo.ts
@@ -1,6 +1,7 @@
 import PrimesWidget from "./PrimesWidget";
 import { BasicProjector, Projection } from "parsegraph-projector";
-import {GraphPainter} from 'parsegraph-graphpainter';
+import { GraphPainter } from "parsegraph-graphpainter";
+import TimingBelt from "parsegraph-timingbelt";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("demo");
@@ -15,60 +16,5 @@ document.addEventListener("DOMContentLoaded", () => {
   const proj = new Projection(projector, painter);
   belt.addRenderable(proj);
 
-  const container = document.createElement("div");
-  container.innerHTML = `${todo()}`;
-  container.style.position = "absolute";
-  container.style.left = "0px";
-  container.style.top = "0px";
-  container.style.pointerEvents = "none";
-  root.appendChild(container);
-  container.style.fontSize = "18px";
-  container.style.fontFamily = "sans";
-  const refresh = () => {
-    const rand = () => Math.floor(Math.random() * 255);
-    document.body.style.backgroundColor = `rgb(${rand()}, ${rand()}, ${rand()})`;
-    container.style.color = `rgb(${rand()}, ${rand()}, ${rand()})`;
-    container.style.left = `${Math.random() * root.clientWidth}px`;
-    container.style.top = `${Math.random() * root.clientHeight}px`;
-  };
-
-  const dot = document.createElement("div");
-  dot.style.position = "absolute";
-  dot.style.right = "8px";
-  dot.style.top = "8px";
-  dot.style.width = "16px";
-  dot.style.height = "16px";
-  dot.style.borderRadius = "8px";
-  dot.style.transition = "background-color 400ms";
-  dot.style.backgroundColor = "#222";
-  root.appendChild(dot);
-
-  container.style.transition = "color 2s, left 2s, top 2s";
-  document.body.style.transition = "background-color 2s";
-  let timer: any = null;
-  let dotTimer: any = null;
-  let dotIndex = 0;
-  const dotState = ["#f00", "#c00"];
-  const refreshDot = () => {
-    dotIndex = (dotIndex + 1) % dotState.length;
-    dot.style.backgroundColor = dotState[dotIndex];
-  };
-  const interval = 3000;
-  const dotInterval = 500;
-  root.addEventListener("click", () => {
-    if (timer) {
-      clearInterval(timer);
-      timer = null;
-      clearInterval(dotTimer);
-      dotTimer = null;
-      dot.style.transition = "background-color 3s";
-      dot.style.backgroundColor = "#222";
-    } else {
-      refresh();
-      dot.style.transition = "background-color 400ms";
-      refreshDot();
-      timer = setInterval(refresh, interval);
-      dotTimer = setInterval(refreshDot, dotInterval);
-    }
-  });
+  root.appendChild(projector.container());
 });
